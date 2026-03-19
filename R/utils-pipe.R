105fd03080db7b9c50c44441836e01419a684af8
Andreas Mändle <AndemWue@gmail.com>
2026-03-19 23:58:21 +0100

    solved some CRAN checks

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -1,14 +1,14 @@
 #' Pipe operator
 #'
-#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#' See \code{\link[magrittr:pipe]{\%>\%}} for details.
 #'
-#' @name %>%
-#' @rdname pipe
+#' @name pipe
+#' @aliases %>%
 #' @keywords internal
-#' @export
-#' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
 #' @param lhs A value or the magrittr placeholder.
-#' @param rhs A function call using the magrittr semantics.
-#' @return The result of calling `rhs(lhs)`.
+#' @param rhs A function call using magrittr semantics.
+#' @return The result of applying \code{rhs} to \code{lhs}.
+#' @export
+#' @importFrom magrittr %>%
 NULL
