dbaac4095bbcebc189826a1d7deaf14420dbb6a9
amaendle <AndemWue@gmail.com>
2026-04-10 23:02:13 +0200

    Update utils-pipe.R

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -1,14 +1,3 @@
-#' Pipe operator
-#'
-#' See \code{\link[magrittr:pipe]{\%>\%}} for details.
-#'
-#' @name pipe
-#' @aliases %>%
-#' @keywords internal
-#' @usage lhs \%>\% rhs
-#' @param lhs A value or the magrittr placeholder.
-#' @param rhs A function call using magrittr semantics.
-#' @return The result of applying \code{rhs} to \code{lhs}.
-#' @export
 #' @importFrom magrittr %>%
-NULL
+#' @export
+magrittr::`%>%`
